cf8d36fcc5963f0fada8ec18b932a2aacfd8cd8a
Mohamed Nasser <159939141+MohamedNasser525@users.noreply.github.com>
2026-04-24 16:55:13 +0300

    Delete model directory

--- model/Engine.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public abstract class Engine {
-    // Shared property for all engines
-    public int speed = 0;
-
-    public abstract void increase();
-    public abstract void decrease();
-}
